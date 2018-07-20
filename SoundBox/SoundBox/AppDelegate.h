13ccffb725c5b2df32fc45a6e4a6ef802d9cec42
Pinto Raphaël <rpinto@dealabs.com>
2018-07-20 09:32:36 +0200

    first commit

---
+++ SoundBox/SoundBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoundBox
+//
+//  Created by rpinto on 20/07/2018.
+//  Copyright © 2018 pepper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoundBox/SoundBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundBox
+//
+//  Created by rpinto on 20/07/2018.
+//  Copyright © 2018 pepper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
